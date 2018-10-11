a574339f9d86c661583987a05f72b033e5ec89e0
methanol <methanol.b@gmail.com>
2018-10-11 17:08:31 +0700

    Task 11 - corrections 1

--- src/server-start-task.js
+++ src/server-start-task.js
@@ -12,32 +12,25 @@ const getStat = promisify(fs.stat);
 const getDir = promisify(fs.readdir);
 const getFile = promisify(fs.readFile);
 
-const hostname = `127.0.0.1`;
-const port = 3000;
+const HOST_NAME = `127.0.0.1`;
+const PORT = 3000;
 const MESSAGE = `Server started`;
 const DESCRIPTION = `connection with server`;
 
-const readFile = async (path, res, type) => {
+async function readFile(path, res, type) {
   const data = await getFile(path);
   res.setHeader(`Content-Type`, type);
   res.end(data);
-};
+}
 
+async function readDir(path, relativePath, res, serverAddress) {
+  const files = await getDir(path);
+  res.setHeader(`content-type`, `text/html`);
+  res.end(showDirectory(path, relativePath, files, serverAddress));
+}
 
-class ServerStartTask extends BaseTask {
-  constructor() {
-    super(currentTask, DESCRIPTION, MESSAGE);
-  }
-
-  execute(servPort = port) {
-    super.execute();
-
-    const validPort = ((typeof servPort === `number`) && (servPort >= 0) && (servPort <= 65535)) ? servPort : port;
-
-    const serverAddress = `http://${hostname}:${validPort}/`;
-
-    function showDirectory(path, relativePath, files) {
-      return `<!DOCTYPE html>
+function showDirectory(path, relativePath, files, serverAddress) {
+  return `<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -49,60 +42,68 @@ class ServerStartTask extends BaseTask {
 </ul>
 </body>
 </html>`;
+}
+
+async function liftServer(req, res, serverAddress) {
+  const localPath = url.parse(req.url).pathname;
+  const absolutePath = process.cwd() + `/static` + localPath;
+
+  try {
+
+    const pathStat = await getStat(absolutePath);
+
+    res.statusCode = 200;
+
+    if (pathStat.isDirectory()) {
+      await readDir(absolutePath, localPath, res, serverAddress);
+    } else {
+      let type = `text/plain`;
+
+      switch (dir.extname(absolutePath)) {
+        case `.html`:
+          type = `text/html`;
+          break;
+        case `.ico`:
+          type = `image/x-icon`;
+          break;
+        case `.css`:
+          type = `text/css`;
+          break;
+        case `.png`:
+          type = `image/png`;
+          break;
+        case `.svg`:
+          type = `image/svg+xml`;
+          break;
+        case `.jpg`:
+          type = `image/jpeg`;
+          break;
+        default:
+          type = `text/plain`;
+      }
+      await readFile(absolutePath, res, type);
     }
+  } catch (e) {
+    res.writeHead(404, `Not Found`);
+    res.end();
+  }
+}
 
-    const readDir = async (path, relativePath, res) => {
-      const files = await getDir(path);
-      res.setHeader(`content-type`, `text/html`);
-      res.end(showDirectory(path, relativePath, files));
-    };
 
+class ServerStartTask extends BaseTask {
+  constructor() {
+    super(currentTask, DESCRIPTION, MESSAGE);
+  }
+
+  execute(serverPort = PORT) {
+    super.execute();
+
+    const validPort = ((typeof serverPort === `number`) && (serverPort >= 0) && (serverPort <= 65535)) ? serverPort : PORT;
+
+    const serverAddress = `http://${HOST_NAME}:${validPort}/`;
     const server = http.createServer((req, res) => {
 
-      (async () => {
-        const localPath = url.parse(req.url).pathname;
-        const absolutePath = process.cwd() + `/static` + localPath;
-
-        try {
-
-          const pathStat = await getStat(absolutePath);
-
-          res.statusCode = 200;
-
-          if (pathStat.isDirectory()) {
-            await readDir(absolutePath, localPath, res);
-          } else {
-            let type = `text/plain`;
-
-            switch (dir.extname(absolutePath)) {
-              case `.html`:
-                type = `text/html`;
-                break;
-              case `.ico`:
-                type = `image/x-icon`;
-                break;
-              case `.css`:
-                type = `text/css`;
-                break;
-              case `.png`:
-                type = `image/png`;
-                break;
-              case `.svg`:
-                type = `image/svg+xml`;
-                break;
-              case `.jpg`:
-                type = `image/jpeg`;
-                break;
-              default:
-                type = `text/plain`;
-            }
-            await readFile(absolutePath, res, type);
-          }
-        } catch (e) {
-          res.writeHead(404, `Not Found`);
-          res.end();
-        }
-      })().catch((e) => {
+      liftServer(req, res, serverAddress).catch((e) => {
         res.writeHead(500, e.message, {
           'content-type': `text/plain`
         });
@@ -111,10 +112,12 @@ class ServerStartTask extends BaseTask {
 
     });
 
-    server.listen(validPort, hostname, () => {
-      console.log(`Server running at http://${hostname}:${validPort}/`);
+    server.listen(validPort, HOST_NAME, () => {
+      console.log(`Server running at http://${HOST_NAME}:${validPort}/`);
     });
   }
 }
+
+new ServerStartTask().execute(3333);
 
 module.exports = ServerStartTask;
